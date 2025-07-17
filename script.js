f0cbef8e82ad22ae474b3bfd9578da9f7807dea1
khushbooagarwall <threesisters3331@gmail.com>
2025-07-18 03:35:07 +0530

    Initial Files

---
+++ script.js
@@ -0,0 +1,17 @@
+// const aside = document.getElementsByTagName("aside");
+// const menu = document.getElementsByClassName("menu");
+// const menuIcon = menu.querySelector("i");
+
+// function sideMenu() {
+//   if (aside.style.display === "none" || menuIcon.classList.contains("ri-menu-line")) 
+//     {
+//         aside.style.display = "block";
+//         menuIcon.classList.add("ri-close-line");
+//         menuIcon.classList.remove("ri-menu-line");
+//     } 
+//     else {
+//         aside.style.display = "none";
+//         menuIcon.classList.remove("ri-close-line");
+//         menuIcon.classList.add("ri-menu-line");
+//     }
+// }
